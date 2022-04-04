fe107317a92197fd780d958c5972230a1cfb3ae1
EricChukoltyansky <kazohelmet@gmail.com>
2022-04-04 18:13:49 +0300

    type inference with state

--- src/states/GuestList.tsx
+++ src/states/GuestList.tsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
 
-const GuestList: React.FC = () => {
-  const [name, setName] = useState("");
+interface Guest {
+  name: string;
+  isConfirmed: boolean;
+  isEditing: boolean;
+  guests: string;
+}
+
+const GuestList: React.FC<Guest> = () => {
+  const [name, setName] = useState<string>("");
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const onClick = () => {
+    setGuests([...guests, name]);
+    setName("");
+  };
 
   return (
     <div>
-      <h3> GuestList</h3>
-      <input value={name} onChange={(e) => setName(e.target.value)}/>
+      <h3>GuestList</h3>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
       <button>Add Guest</button>
     </div>
   );
