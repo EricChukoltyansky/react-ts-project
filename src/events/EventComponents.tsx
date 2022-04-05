ba720ed227d5b8f4a898670c44d889ba623717d8
EricChukoltyansky <kazohelmet@gmail.com>
2022-04-05 20:49:39 +0300

    drag events

--- src/events/EventComponents.tsx
+++ src/events/EventComponents.tsx
@@ -1,11 +1,16 @@
 const EventComponent: React.FC<{}> = () => {
-  const onChange = (e: any) => {
-    console.log(e);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+  };
+
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    console.log(event);
   };
 
   return (
     <div>
       <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}></div>
     </div>
   );
 };
