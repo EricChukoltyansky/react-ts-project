a959a1f5eecf942ea1e8ab4a38314f06cb39a6db
EricChukoltyansky <kazohelmet@gmail.com>
2022-04-24 19:53:07 +0300

    create reducers

---
+++ src/states/reducers/repoReducers.tsx
@@ -0,0 +1,35 @@
+interface RepoState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (state: RepoState, action: any) => {
+  switch (action.type) {
+    case "search_repos":
+      return {
+        ...state,
+        loading: true,
+        error: null,
+        data: [],
+      };
+    case "search_repos_success":
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        data: action.payload,
+      };
+    case "search_repos_failure":
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+        data: [],
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
