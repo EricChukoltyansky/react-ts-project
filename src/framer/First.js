96829769b3b85992feb8b4751bf5e274256f9991
EricChukoltyansky <kazohelmet@gmail.com>
2022-05-22 21:04:49 +0300

    rotation

--- src/framer/First.js
+++ src/framer/First.js
@@ -3,11 +3,14 @@ import { motion } from "framer-motion";
 import "./First.css";
 
 function First() {
+  const [rotate, setRotate] = React.useState(false);
   return (
     <div className="example-container">
-      <motion.div>
-          
-      </motion.div>
+      <motion.div
+        animate={{ rotate: rotate ? 360 : 0 }}
+        transition={{ duration: 4 }}
+        onClick={() => setRotate(!rotate)}
+      >Click me</motion.div>
     </div>
   );
 }
