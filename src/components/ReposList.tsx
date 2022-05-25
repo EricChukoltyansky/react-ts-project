7ca39de625ce4bc6da8005ad8185a2f32d8deac9
EricChukoltyansky <kazohelmet@gmail.com>
2022-05-25 19:17:06 +0300

    adding type to form event

--- src/components/ReposList.tsx
+++ src/components/ReposList.tsx
@@ -1,8 +1,18 @@
+import e from 'express';
+import {useState} from 'react';
+
 const ReposList: React.FC = () => {
+  const [term, setTerm] = useState('');
+
+const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+  console.log(term);
+}
+
   return (
     <>
-      <form>
-        <input type="text" />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={ e => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </>
