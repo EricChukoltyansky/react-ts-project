708faa3f134dcc64d4b35e168af4cb3ee4a2a7ad
EricChukoltyansky <kazohelmet@gmail.com>
2022-05-25 19:44:35 +0300

    useActions

--- src/components/ReposList.tsx
+++ src/components/ReposList.tsx
@@ -1,18 +1,20 @@
-import e from 'express';
-import {useState} from 'react';
+import { useState } from "react";
+import { useActions } from "../hooks/useActions";
 
 const ReposList: React.FC = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("");
+  const { searchRepos } = useActions();
 
-const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  event.preventDefault();
-  console.log(term);
-}
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(term);
+  };
 
+  searchRepos(term);
   return (
     <>
       <form onSubmit={onSubmit}>
-        <input value={term} onChange={ e => setTerm(e.target.value)} />
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </>
